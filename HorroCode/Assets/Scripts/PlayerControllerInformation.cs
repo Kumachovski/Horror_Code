6b450e11b454ff96643f32a830d5b70bb5dd5c5e
Kumachovski <artturi.lobski@gmail.com>
2020-02-02 04:00:19 +0200

    Added blinking mechanics

--- HorroCode/Assets/Scripts/PlayerControllerInformation.cs
+++ HorroCode/Assets/Scripts/PlayerControllerInformation.cs
@@ -6,27 +6,40 @@ using UnityEngine.UI;
 
 public class PlayerControllerInformation : MonoBehaviour
 {
-	private float coffee;
-	public float food;
-	private float timer;
+	
 	public GameObject hunger;
 	public GameObject coffeine;
-	
+    public Animator animator;
+    
+    public float coffee;
+    public float food;
+    public float timer;
+    public float timer2;
+    private float movetime;
+    private bool eyeState; //false = open true = closed;
+    private float startTimer;
+    private Animation anim;
     // Start is called before the first frame update
     void Start()
     {
+     
 		timer += Time.deltaTime;
-		coffee = 100.0f;
+        timer2 = 0f;
+        coffee = 100.0f;
 		food = 100.0f;
-        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-	food -= timer;
-	coffee -= timer;
-	coffeine.transform.localScale = new Vector3(coffee/10f,coffeine.transform.localScale.y,coffeine.transform.localScale.z);
-	hunger.transform.localScale = new Vector3(food/10f,hunger.transform.localScale.y,hunger.transform.localScale.z);
+	    food -= timer;
+	    coffee -= timer;
+        timer2 -= timer;
+	    coffeine.transform.localScale = new Vector3(coffee/10f,coffeine.transform.localScale.y,coffeine.transform.localScale.z);
+	    hunger.transform.localScale = new Vector3(food/10f,hunger.transform.localScale.y,hunger.transform.localScale.z);
+
+        animator.SetFloat("SleepyState", coffee);
 	}
+
 }
