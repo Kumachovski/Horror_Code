285e2733addb8daf2dc4327cb95ca74a5b4934ed
Kumachovski <artturi.lobski@gmail.com>
2020-02-02 14:23:53 +0200

    added player hide mechanics

---
+++ HorroCode/Assets/Scripts/TriggerPlayerHide.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPlayerHide : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        player.GetComponent<PlayerControllerInformation>().hiddenState = true;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        player.GetComponent<PlayerControllerInformation>().hiddenState = false;
+    }
+}
